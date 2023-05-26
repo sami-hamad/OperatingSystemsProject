9330b1d6d911e7ab8e6d5a62d409cd7f642ec245
sami-hamad <sami.mohd.hamad@gmail.com>
2023-05-26 22:31:23 +0300

    Replaced getting input from files to getting input from user, and added comments for readability

--- OS_Project/OS_Project.cpp
+++ OS_Project/OS_Project.cpp
@@ -2,8 +2,8 @@
 #include <fstream>
 using namespace std;
 
-ofstream fout("output.txt");
-ifstream fin("input.txt");
+//ofstream fout("output.txt");
+//ifstream fin("input.txt");
 
 void fillMatrices(int* available, int** max, int** allocation);
 void calculateNeed(int** need, int** max, int** allocation);
@@ -15,19 +15,18 @@ void printSequence(const int* sequence, const bool* finish);
 int PROCESS, RESOURCE;
 
 int main() {
-    cout << "\n\nWelcome, this is an implementation of Banker’s algorithm to "
-            "avoid deadlocks!\n\n";
+    cout << "\n\nWelcome, this is an implementation of Banker's algorithm to avoid deadlocks!\n\n";
     
     do {
         cout << "Enter the number of processes: ";
-        fin >> PROCESS;
+        cin >> PROCESS;
         if (PROCESS < 1)
           cout << "Please enter a valid number of processes (>0)\n";
     } while (PROCESS < 1);
     
     do {
         cout << "Enter the number of resources: ";
-        fin >> RESOURCE;
+        cin >> RESOURCE;
         if (RESOURCE < 1)
           cout << "Please enter a valid number of resources (>1)\n";
     } while (RESOURCE < 1);
@@ -46,33 +45,34 @@ int main() {
         max[i] = new int[RESOURCE];
         need[i] = new int[RESOURCE];
         finish[i] = false;
-        sequence[i] = -1;
+        sequence[i] = -1;//this is useful when there's partial sequences (to know where to stop when printing the sequence) 
+                         //because the sequence matrix is partially filled
     }
     
     
-    //filling available, max, and allocation matrices
+    //filling available, max, and allocation matrices from the user
     fillMatrices(available, max, allocation);
     
-    //filling need matrix
+    //calculating need matrix
     calculateNeed(need, max, allocation);
     
-    //printing matrices
+    //printing all matrices
     printMatrices(need, max, allocation, available);
 
-    //check for safe state
+    //checking for safe state
     bool safe = isSafeState(need, allocation, available, finish, sequence);
 
 
-    //printing the sequence if the system is in a safe state
+    //if the system is in a safe state
     if (safe) {
-        fout << "\n\nThe System is in a safe state, and the sequence is ";
+        cout << "\n\nThe System is in a safe state, and the sequence is ";
         printSequence(sequence, finish);
     }
-    else {
-        if(!partialSequence(finish))//to check if there's a partial sequence or not
-            fout << "\n\nThe system is in an unsafe, and there's no partial sequence.";
+    else {//system is not safe
+        if(!partialSequence(finish))//if there's a partial sequence or not
+            cout << "\n\nThe system is in an unsafe, and there's no partial sequence.";
         else {
-            fout << "\n\nThe system is in an unsafe state, but the partial sequence is ";
+            cout << "\n\nThe system is in an unsafe state, but the partial sequence is ";
             printSequence(sequence, finish);
         }
     }
@@ -99,7 +99,7 @@ void fillMatrices(int* available, int** max, int** allocation) {
         cout << "Process " << i + 1 << ":\n";
         for (int j = 0; j < RESOURCE; ++j) {
             cout << "\t\t\tResource " << (char)(j + 65) << ": ";
-            fin >> allocation[i][j];
+            cin >> allocation[i][j];
         }
     }
 
@@ -108,14 +108,14 @@ void fillMatrices(int* available, int** max, int** allocation) {
         cout << "Process " << i + 1 << ":\n";
         for (int j = 0; j < RESOURCE; ++j) {
             cout << "\t\t\tResource " << (char)(j + 65) << ": ";
-            fin >> max[i][j];
+            cin >> max[i][j];
         }
     }
 
     cout << "Enter the number of available instances of each resource:\n";
     for (int i = 0; i < RESOURCE; ++i) {
         cout << "Resource " << (char)(i + 65) << ": ";
-        fin >> available[i];
+        cin >> available[i];
     }
 }
 void calculateNeed(int** need, int** max, int** allocation) {
@@ -126,77 +126,78 @@ void calculateNeed(int** need, int** max, int** allocation) {
     }
 }
 void printMatrices(int** need, int** max, int** allocation, int* available) {
-    fout << "\nALLOCATED MATRIX:\n";
+    cout << "\nALLOCATED MATRIX:\n";
     for (int i = 0; i < PROCESS; ++i) {
-        fout << "P" << i << " : ";
+        cout << "P" << i << " : ";
         for (int j = 0; j < RESOURCE; ++j) {
-            fout << allocation[i][j] << "\t";
+            cout << allocation[i][j] << " ";
         }
-        fout << "\n";
+        cout << "\n";
     }
 
-    fout << "\nMAX MATRIX:\n";
+    cout << "\nMAX MATRIX:\n";
     for (int i = 0; i < PROCESS; ++i) {
-        fout << "P" << i << " : ";
+        cout << "P" << i << " : ";
         for (int j = 0; j < RESOURCE; ++j) {
-            fout << max[i][j] << "\t";
+            cout << max[i][j] << " ";
         }
-        fout << "\n";
+        cout << "\n";
     }
 
-    fout<<"\nNEED MATRIX:\n";
+    cout<<"\nNEED MATRIX:\n";
     for (int i = 0; i < PROCESS; ++i) {
-        fout << "P" << i << " : ";
+        cout << "P" << i << " : ";
        for (int j = 0; j < RESOURCE; ++j) {
-         fout << need[i][j]<<"\t";
+           cout << need[i][j] << " ";
        }
-       fout<<"\n";
+       cout<<"\n";
     }
 }
 
 bool isSafeState(int** need, int** allocation,int* available, bool* finish,int *sequence) {
 
     int* work = new int[RESOURCE];
-    fout << "\n\nWORK:" << endl;
+    cout << "\n\nWORK:" << endl;
     for (int w = 0; w < RESOURCE; w++) {
         work[w] = available[w];
-        fout << work[w] << " ";
+        cout << work[w] << " ";
     }
-    fout << endl;
+    cout << endl;
 
     int counter = 0;
-    while (counter<PROCESS) {
+    while (counter<PROCESS) {//if all processes where executed successfully (all finish[] = true)
 
-        bool O = false;//flag to indicate if there's no any valid processes through one iteration
+        bool O = false;//flag to indicate if there's no any valid processes through one iteration 
 
         for (int i = 0; i < PROCESS; i++) {
             if (!finish[i]) {
-                bool needlessthanwork = true; 
+                bool needlessthanwork = true;
                 for (int j = 0; j < RESOURCE; j++) {
                     if (need[i][j] > work[j]) {
                         needlessthanwork = false;
                         break;
                     }
                 }
-                if (needlessthanwork) {
+                if (needlessthanwork) {//if the need resources of a process is less than the work resources
                     sequence[counter] = i;
                     counter++;
                     finish[i] = true;
                     O = true;
                     for (int q = 0; q < RESOURCE; q++) {
                         work[q] += allocation[i][q];
-                        fout << work[q] << " ";
+                        cout << work[q] << " ";
                     }
-                    fout << endl;
+                    cout << endl;
                 }
             }
         }
-        if (!O) {
+        if (!O) {//there were no any executed process after one whole scan
             delete[]work;
             return false;
         }
     }
 
+    //all the process where successfully executed (safe state system)
     delete[]work;
     return true;
 
@@ -208,11 +209,11 @@ bool partialSequence(const bool* finish) {
     return false;
 }
 void printSequence(const int* sequence, const bool* finish) {
-    fout << "< ";
+    cout << "< ";
     for (int i = 0; i < PROCESS; i++) {
         if (sequence[i]>-1) {
-            fout << "P" << sequence[i]<< " ";
+            cout << "P" << sequence[i]<< " ";
         }
     }
-    fout << ">\n";
+    cout << ">\n";
 }
